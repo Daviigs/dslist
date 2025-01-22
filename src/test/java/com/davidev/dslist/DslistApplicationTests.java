693859e15fa823ca60fead2837e54366a30b45e7
Davi Miquéias <miqueiasdavi8@gmail.com>
2025-01-22 11:22:02 -0300

    Projeto criado

---
+++ src/test/java/com/davidev/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidev.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
